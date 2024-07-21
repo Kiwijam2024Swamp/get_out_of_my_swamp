0674012c5418b6fae88fc417bb90185a84e7cc99
Ithrendrial <nsx9698@autuni.ac.nz>
2024-07-21 14:12:57 +1200

    feat: breacher damage interaction with soundwaves, breacher "death", and fix swamp home collider

--- Assets/Scripts/BreacherController.cs
+++ Assets/Scripts/BreacherController.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BreacherController : MonoBehaviour
 {
+    public SpriteRenderer sr;
+    public Rigidbody2D rb;  
     public float speed = 5.0f;
     public float maxHealth = 3.0f;
     private Transform _targetPos;
-
+    private Transform _offscreenTargetPos;
     private float _health;
+    private Boolean _damageEffect;
 
 
 
@@ -16,22 +20,46 @@ public class BreacherController : MonoBehaviour
     void Start()
     {
         _health = maxHealth;
+        _damageEffect = false;
         _targetPos = GameObject.FindGameObjectWithTag("BreacherTarget").transform;
+        _offscreenTargetPos = GameObject.FindGameObjectWithTag("OffscreenBreacherTarget").transform;
+
+        sr = this.GetComponentInChildren<SpriteRenderer>();
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, _targetPos.position, speed * Time.deltaTime);
-
-        if(_health <= 0)
+        if(_health > 0 && _damageEffect == false)
         {
-            Destroy(gameObject);
+            //Move towards house door
+            transform.position = Vector2.MoveTowards(transform.position, _targetPos.position, speed * Time.deltaTime);
+        } else {
+            //Move away from house door
+            transform.position = Vector2.MoveTowards(transform.position, _offscreenTargetPos.position, speed * Time.deltaTime);
         }
     }
 
     public void TakeDamage(float damage)
     {
         _health -= damage;
+        _damageEffect = true;
+        StartCoroutine(DamageEffect());
     } 
+
+    IEnumerator DamageEffect()
+    {
+        speed = 0.0f;
+        for(int i = 0; i < 3; i++)
+            {
+                sr.color = Color.red;
+                yield return new WaitForSeconds (0.1f); 
+                sr.color = Color.white;
+                yield return new WaitForSeconds (0.1f); 
+            }
+        yield return new WaitForSeconds (0.5f); 
+        speed = 5.0f;
+        _damageEffect = false;
+    }
 }
