1776cdcf5ebd6c42598271f6b19977a72933a1f0
Moni <thaker.moni@gmail.com>
2024-07-21 16:23:42 +1200

    fix: backup build

--- Assets/Scripts/HomeController.cs
+++ Assets/Scripts/HomeController.cs
@@ -1,23 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.SceneManagement;
-
 
 public class HomeController : MonoBehaviour
 {
     private int _breacherCount = 0;
     public int maxBreacherCount = 10;
-    private bool isDoneWaiting = false;
-    private bool gameOver = false;
-    public GameObject gameOverSprite; // Reference to the GameObject with the sprite
-    private SpriteRenderer spriteRenderer;
-    public float fadeDuration = 1f; // Duration of the fade
-    private float fadeTimer = 0f;
-
-
 
     //HP Related UI
     [SerializeField] private TextMeshProUGUI _breachText;
@@ -29,13 +20,6 @@ public class HomeController : MonoBehaviour
         _breacherCount = 0;
         _breachSlider.value = 0;
         _breachSlider.maxValue = maxBreacherCount;
-
-        // spriteRenderer = gameOverSprite.GetComponent<SpriteRenderer>();
-
-        // Make sure the sprite starts fully invisible
-        Color color = spriteRenderer.color;
-        color.a = 0f;
-        spriteRenderer.color = color;
     }
 
     // Update is called once per frame
@@ -48,30 +32,10 @@ public class HomeController : MonoBehaviour
 
         _breachText.text = _breacherCount + "/" + maxBreacherCount;
 
-        // Game over!!
-        if (_breachSlider.value >= _breachSlider.maxValue)
+        if (_breacherCount >= maxBreacherCount)
         {
-            GameOverWaiting();
+            SceneManager.LoadScene("TitleScreen");
         }
-        if (isDoneWaiting == true)
-        {
-            SwitchToScene("TitleScreen");
-        }
-
-        // If game over, gradually increase opacity
-        if (gameOver)
-        {
-            fadeTimer += Time.deltaTime;
-            float alpha = Mathf.Clamp01(fadeTimer / fadeDuration);
-            Color color = spriteRenderer.color;
-            color.a = alpha;
-            spriteRenderer.color = color;
-        }
-    }
-
-    public void SwitchToScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
     }
 
     public void AddBreacher()
@@ -87,19 +51,5 @@ public class HomeController : MonoBehaviour
             AddBreacher();
             Destroy(col.gameObject);
         }
-    }
-
-    IEnumerator GameOverWaiting()
-    {
-        gameOver = true;
-        //Print the time of when the function is first called.
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
-
-        //yield on a new YieldInstruction that waits for 5 seconds.
-        yield return new WaitForSeconds(5);
-
-        //After we have waited 5 seconds print the time again.
-        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
-        isDoneWaiting = true;
     }
 }
