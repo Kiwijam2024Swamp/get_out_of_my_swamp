797cac4f345a4d1a62ade8abffbebe88ffd86a0f
nsamelson <salocin.sam@gmail.com>
2024-07-21 14:02:07 +1200

    game over thingy (need to link to the component yet)

--- Assets/Scripts/HomeController.cs
+++ Assets/Scripts/HomeController.cs
@@ -10,6 +10,14 @@ public class HomeController : MonoBehaviour
 {
     private int _breacherCount = 0;
     public int maxBreacherCount = 10;
+    private bool isDoneWaiting = false;
+    private bool gameOver = false;
+    public GameObject gameOverSprite; // Reference to the GameObject with the sprite
+    private SpriteRenderer spriteRenderer;
+    public float fadeDuration = 1f; // Duration of the fade
+    private float fadeTimer = 0f;
+
+
 
     //HP Related UI
     [SerializeField] private TextMeshProUGUI _breachText;
@@ -21,6 +29,13 @@ public class HomeController : MonoBehaviour
         _breacherCount = 0;
         _breachSlider.value = 0;
         _breachSlider.maxValue = maxBreacherCount;
+
+        spriteRenderer = gameOverSprite.GetComponent<SpriteRenderer>();
+
+        // Make sure the sprite starts fully invisible
+        Color color = spriteRenderer.color;
+        color.a = 0f;
+        spriteRenderer.color = color;
     }
 
     // Update is called once per frame
@@ -35,9 +50,20 @@ public class HomeController : MonoBehaviour
 
         // Game over!!
         if(_breachSlider.value >= _breachSlider.maxValue){
-
-
+            GameOverWaiting();
+        }
+        if (isDoneWaiting == true){
             SwitchToScene("TitleScreen");
+        }
+
+        // If game over, gradually increase opacity
+        if (gameOver)
+        {
+            fadeTimer += Time.deltaTime;
+            float alpha = Mathf.Clamp01(fadeTimer / fadeDuration);
+            Color color = spriteRenderer.color;
+            color.a = alpha;
+            spriteRenderer.color = color;
         }
     }
 
@@ -56,5 +82,19 @@ public class HomeController : MonoBehaviour
             AddBreacher();
             Destroy(col.gameObject);
         }
+    }
+
+    IEnumerator GameOverWaiting()
+    {
+        gameOver = true;
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        isDoneWaiting = true;
     }
 }
